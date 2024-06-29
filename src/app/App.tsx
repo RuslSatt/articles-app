c27b7b0c849a88a5bdbedd9f05db39bf91b46976
Ruslan Sattarov <r8sattarov@mail.ru>
2024-06-29 13:18:27 +0500

    build: Add FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,10 @@ import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {lazy, Suspense} from "react";
 import {useTheme} from "@/app/providers/theme";
-import {classNames} from "@/helpers/classNames/classNames";
+import {classNames} from "@/shared/lib/classNames/classNames";
 
-const MainPage = lazy(() => import("@/components/MainPage/MainPage"));
-const AboutPage = lazy(() => import("@/components/AboutPage/AboutPage"));
+const MainPage = lazy(() => import("@/pages/MainPage"));
+const AboutPage = lazy(() => import("@/pages/AboutPage"));
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -27,7 +27,6 @@ const App = () => {
                     <Route path="/about" element={<AboutPage />} />
                 </Routes>
             </Suspense>
-
         </div>
     );
 };
