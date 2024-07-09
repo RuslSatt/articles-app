e93e9f5f7031f7ebf2befee00b9e28e4da3b7161
Ruslan Sattarov <r8sattarov@mail.ru>
2024-07-09 21:44:21 +0500

    build: Add bundle analyzer

--- config/buildPlugins.ts
+++ config/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { IBuildOptions } from './types/config';
 
 export function buildPlugins(options: IBuildOptions): webpack.WebpackPluginInstance[] {
@@ -16,6 +17,9 @@ export function buildPlugins(options: IBuildOptions): webpack.WebpackPluginInsta
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 }
