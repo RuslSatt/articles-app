def55ec8f845406ac236eb575ba0a23950830cd5
Ruslan Sattarov <r8sattarov@mail.ru>
2024-07-10 22:09:55 +0500

    build: Configuration jest dom

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -9,14 +9,22 @@ const config: Config = {
     clearMocks: true,
     coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
     coverageProvider: 'v8',
-    moduleDirectories: ['node_modules', 'src'],
+    moduleDirectories: ['node_modules'],
     moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
     rootDir: '../../',
+    modulePaths: ['<rootDir>src'],
     testEnvironment: 'jsdom',
     testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
     testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+    setupFilesAfterEnv: ['<rootDir>config/jest/jestSetup.ts'],
     moduleNameMapper: {
-        '@/(.*)': '<rootDir>src/$1'
+        '@/(.*)': '<rootDir>src/$1',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    },
+    transform: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>config/jest/fileTransformer.js',
+        '^.+\\.[t|j]sx?$': 'babel-jest'
     }
 };
 
