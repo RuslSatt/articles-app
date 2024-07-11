64b75dc26111751fbb31a808bb7c0a8ca6704a12
Ruslan Sattarov <r8sattarov@mail.ru>
2024-07-11 20:59:17 +0500

    build: Add support useTranslation for jest

--- src/widgets/Navbar/ui/Navbar.test.tsx
+++ src/widgets/Navbar/ui/Navbar.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { Navbar } from './Navbar';
+import { renderWithTranslation } from '@/shared/lib/renderWithTranslation/renderWithTranslation';
 
 describe('Navbar component', () => {
     test('should render', () => {
-        render(<Navbar />);
+        renderWithTranslation(<Navbar />);
         expect(screen.getByRole('header')).toBeInTheDocument();
     });
 });
