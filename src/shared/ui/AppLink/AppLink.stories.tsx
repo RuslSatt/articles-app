f5ca8c2c65684033b7f93fcb1c295f0bfa500651
Ruslan Sattarov <r8sattarov@mail.ru>
2024-07-13 12:59:42 +0500

    build: Add stories for all widgets and shared ui

---
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { AppLink } from './AppLink';
+import { ThemeDecorator } from '@/shared/config/storybook/decorators/ThemeDecorator';
+import { Theme } from '@/app/providers/theme';
+
+const meta = {
+    title: 'shared/AppLink',
+    component: AppLink,
+    parameters: {
+        layout: 'centered'
+    },
+    args: {
+        children: 'Link',
+        to: '/'
+    },
+    tags: ['autodocs']
+} satisfies Meta<typeof AppLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {}
+};
+
+export const Dark: Story = {
+    args: {}
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ src/shared/ui/Loader/Loader.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Loader } from './Loader';
+import { ThemeDecorator } from '@/shared/config/storybook/decorators/ThemeDecorator';
+import { Theme } from '@/app/providers/theme';
+
+const meta = {
+    title: 'shared/Loader',
+    component: Loader,
+    parameters: {
+        layout: 'centered'
+    },
+
+    tags: ['autodocs']
+} satisfies Meta<typeof Loader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {}
+};
+
+export const Dark: Story = {
+    args: {}
+};
+
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
