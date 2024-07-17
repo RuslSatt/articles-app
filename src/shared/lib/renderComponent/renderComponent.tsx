9baebdab9bfd657731d9afb0a35dd5d120b26950
Ruslan Sattarov <r8sattarov@mail.ru>
2024-07-17 19:54:52 +0500

    feat: Add options for render component in tests

--- src/shared/lib/renderComponent/renderComponent.tsx
+++ src/shared/lib/renderComponent/renderComponent.tsx
@@ -4,9 +4,15 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import i18n from '@/shared/config/i18n/i18nTest';
 
-export function renderComponent(component: ReactNode) {
+export interface RenderComponentOptions {
+    route?: string;
+}
+
+export function renderComponent(component: ReactNode, options: RenderComponentOptions = {}) {
+    const { route = '/' } = options;
+
     return render(
-        <MemoryRouter initialEntries={['/']}>
+        <MemoryRouter initialEntries={[route]}>
             <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
         </MemoryRouter>
     );
