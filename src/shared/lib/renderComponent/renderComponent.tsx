ee2012eb4b5f898f2b1315ce84bc8df2255f07bb
Ruslan Sattarov <r8sattarov@mail.ru>
2024-07-24 21:25:34 +0500

    feat: Add StoreProvider for tests

--- src/shared/lib/renderComponent/renderComponent.tsx
+++ src/shared/lib/renderComponent/renderComponent.tsx
@@ -2,18 +2,23 @@ import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
+import { DeepPartial } from '@reduxjs/toolkit';
 import i18n from '@/shared/config/i18n/i18nTest';
+import { StateSchema, StoreProvider } from '@/app/providers/store';
 
 export interface RenderComponentOptions {
     route?: string;
+    initialState?: DeepPartial<StateSchema>;
 }
 
 export function renderComponent(component: ReactNode, options: RenderComponentOptions = {}) {
-    const { route = '/' } = options;
+    const { route = '/', initialState } = options;
 
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
-        </MemoryRouter>
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18n}>{component}</I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>
     );
 }
