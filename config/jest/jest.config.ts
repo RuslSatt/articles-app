c50d7cf39e2ef4b46a8e55fee61b20895569fe80
Ruslan Sattarov <r8sattarov@mail.ru>
2024-07-24 21:44:18 +0500

    feat: Define global variables in the jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -26,5 +26,8 @@ export default {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>config/jest/fileTransformer.js',
         '^.+\\.[t|j]sx?$': 'babel-jest'
+    },
+    globals: {
+        __IS_DEV__: true
     }
 };
