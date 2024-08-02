82927e4c7a81653dd1eb5fc15db9b9ee8a3d1e85
Ruslan Sattarov <r8sattarov@mail.ru>
2024-08-02 10:43:46 +0500

    feat: Add test for loginSlice

---
+++ src/feature/login/model/slice/loginSlice.test.ts
@@ -0,0 +1,25 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/login';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('getLoginState', () => {
+    test('should change username', () => {
+        const state: DeepPartial<LoginSchema> = {
+            username: 'admin'
+        };
+
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('123'))).toEqual({
+            username: '123'
+        });
+    });
+
+    test('should change password', () => {
+        const state: DeepPartial<LoginSchema> = {
+            password: '123'
+        };
+
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('admin'))).toEqual({
+            password: 'admin'
+        });
+    });
+});
