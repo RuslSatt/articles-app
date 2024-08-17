c3695151c28f1ca5f6037eff16e1684de26e4a12
Ruslan Sattarov <r8sattarov@mail.ru>
2024-08-17 08:52:13 +0500

    feat: Implement Select UI component

---
+++ src/shared/ui/Select/Select.tsx
@@ -0,0 +1,51 @@
+import React, { memo, useMemo } from 'react';
+import style from './Select.module.scss';
+import { classNames } from '@/shared/lib/classNames/classNames';
+
+interface Option {
+    [key: string]: string;
+}
+
+export interface SelectProps {
+    className?: string;
+    value?: string;
+    onChange?: (value: string) => void;
+    readonly?: boolean;
+    options?: Option[];
+    optionLabel?: string;
+}
+
+export const Select = memo((props: SelectProps) => {
+    const {
+        className,
+        value,
+        onChange = (value) => {},
+        readonly,
+        options = [],
+        optionLabel = 'name'
+    } = props;
+
+    const data = useMemo(
+        () =>
+            options?.map((item) => (
+                <option value={item[optionLabel]} key={item[optionLabel]}>
+                    {item[optionLabel]}
+                </option>
+            )),
+        [options, optionLabel]
+    );
+
+    const handlerChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        onChange?.(e.target.value);
+    };
+
+    return (
+        <select
+            value={value}
+            onChange={handlerChange}
+            className={classNames(style.select, [className])}
+        >
+            {data}
+        </select>
+    );
+});
