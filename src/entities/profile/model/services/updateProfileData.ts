7afd05328b629ceb3d9cdad84a00fc90d3dc3046
Ruslan Sattarov <r8sattarov@mail.ru>
2024-08-17 07:37:17 +0500

    feat: Implement put request for profile

--- src/entities/profile/model/services/updateProfileData.ts
+++ src/entities/profile/model/services/updateProfileData.ts
@@ -1,16 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from '@/app/providers/store';
 import { IProfile } from '../types/profile';
+import { getProfileForm } from '../selectors/getProfileForm';
 
 export const updateProfileData = createAsyncThunk<IProfile, void, ThunkConfig<string>>(
     'profile/updateProfileData',
     async (_, thunkApi) => {
         const { rejectWithValue, extra, getState } = thunkApi;
 
-        const { form } = getState().profile || {};
+        const formData = getProfileForm(getState()) || {};
 
         try {
-            const response = await extra.api.post('/profile', form);
+            const response = await extra.api.put<IProfile>('/profile', formData);
 
             return response.data;
         } catch (e: any) {
