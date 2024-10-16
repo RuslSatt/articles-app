13b8ca5cc84293d6b9a31b6fb40dee60d40555af
Ruslan Sattarov <r8sattarov@mail.ru>
2024-10-16 19:54:09 +0500

    feat: Add code block of article

--- src/entities/article/ui/ArticleCodeBlock/ArticleCodeBlock.tsx
+++ src/entities/article/ui/ArticleCodeBlock/ArticleCodeBlock.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import style from './ArticleCodeBlock.module.scss';
@@ -12,11 +11,9 @@ export interface ArticleCodeBlockProps {
 export const ArticleCodeBlock = memo((props: ArticleCodeBlockProps) => {
     const { className, content } = props;
 
-    const { t } = useTranslation();
-
     return (
         <div className={classNames(style.articleCodeBlock, [className])}>
-            <div>{content.code}</div>
+            <code className={style.code}>{content.code}</code>
         </div>
     );
 });
