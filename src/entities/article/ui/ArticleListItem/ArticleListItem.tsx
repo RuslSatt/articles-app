d2476eb6e033ff81f1a3c1eb7011e5610926f491
Ruslan Sattarov <r8sattarov@mail.ru>
2024-10-23 21:29:51 +0500

    feat: Change styles for grid view article list

--- src/entities/article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/article/ui/ArticleListItem/ArticleListItem.tsx
@@ -14,48 +14,34 @@ export interface ArticleTextBlockProps {
 export const ArticleListItem = memo((props: ArticleTextBlockProps) => {
     const { className, article, view = ArticleView.LIST } = props;
 
-    let renderItem;
-
-    if (view === ArticleView.GRID) {
-        renderItem = () => {
-            return (
-                <div className={classNames(style.item, [className])}>
-                    <Card>
-                        <div className={style.item__container}>
-                            <div className={style.img__container}>
-                                <img
-                                    className={style.img}
-                                    src={article?.img}
-                                    alt={article?.title}
-                                />
-                                <div className={style.date}>{article?.createdAt}</div>
-                            </div>
-                            <div className={style.info__container}>
-                                <div className={style.info}>
-                                    <div className={style.info__type}>{article?.type}</div>
-                                    <div className={style.info__views}>
-                                        <div className={style.info__views_count}>
-                                            {article.views}
-                                        </div>
-                                        <EyeIcon className={style.info__views_icon} />
-                                    </div>
-                                </div>
-                                <div className={style.title}>{article?.title}</div>
-                            </div>
-                        </div>
-                    </Card>
-                </div>
-            );
-        };
-    } else {
-        renderItem = () => {
-            return (
-                <div className={classNames(style.item, [className])}>
-                    {article?.title && <h3 className={style.title}>{article?.title}</h3>}
-                </div>
-            );
-        };
+    if (view === ArticleView.LIST) {
+        return (
+            <div className={classNames(style.item, [className])}>
+                {article?.title && <h3 className={style.title}>{article?.title}</h3>}
+            </div>
+        );
     }
 
-    return renderItem();
+    return (
+        <div className={classNames(style.item, [className])}>
+            <Card>
+                <div className={style.item__container}>
+                    <div className={style.img__container}>
+                        <img className={style.img} src={article?.img} alt={article?.title} />
+                        <div className={style.date}>{article?.createdAt}</div>
+                    </div>
+                    <div className={style.info__container}>
+                        <div className={style.info}>
+                            <div className={style.info__type}>{article?.type}</div>
+                            <div className={style.info__views}>
+                                <div className={style.info__views_count}>{article.views}</div>
+                                <EyeIcon className={style.info__views_icon} />
+                            </div>
+                        </div>
+                        <div className={style.title}>{article?.title}</div>
+                    </div>
+                </div>
+            </Card>
+        </div>
+    );
 });
