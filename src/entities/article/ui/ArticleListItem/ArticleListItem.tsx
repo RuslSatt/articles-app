061bb76a031252525e1234de5430d95b33f038bf
Ruslan Sattarov <r8sattarov@mail.ru>
2024-10-24 18:43:10 +0500

    feat: Add list view type to article

--- src/entities/article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,9 +1,18 @@
 import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import style from './ArticleListItem.module.scss';
-import { ArticleView, IArticle } from '../../model/types/article';
+import {
+    ArticleBlockType,
+    ArticleView,
+    IArticle,
+    IArticleTextBlock
+} from '../../model/types/article';
 import EyeIcon from '@/shared/assets/icons/eye.svg';
 import { Card } from '@/shared/ui/Card/Card';
+import { Avatar } from '@/shared/ui/Avatar/Avatar';
+import { ArticleTextBlock } from '../ArticleTextBlock/ArticleTextBlock';
+import { Button } from '@/shared/ui/Button/Button';
 
 export interface ArticleTextBlockProps {
     className?: string;
@@ -14,16 +23,68 @@ export interface ArticleTextBlockProps {
 export const ArticleListItem = memo((props: ArticleTextBlockProps) => {
     const { className, article, view = ArticleView.LIST } = props;
 
+    const { t } = useTranslation();
+
+    const Types = article.type?.map((type) => (
+        <div className={style.type} key={type}>
+            {type}
+        </div>
+    ));
+
+    const Views = (
+        <div className={style.views}>
+            <div>{article.views}</div>
+            <EyeIcon />
+        </div>
+    );
+
     if (view === ArticleView.LIST) {
+        const contentBlock = article?.blocks?.find((block) => block.type === ArticleBlockType.TEXT);
+
         return (
-            <div className={classNames(style.item, [className])}>
-                {article?.title && <h3 className={style.title}>{article?.title}</h3>}
+            <div className={classNames(style.item, [className, style[view]])}>
+                <Card>
+                    <div className={style.item__container}>
+                        <div className={style.header}>
+                            <div className={style.header__user}>
+                                <Avatar
+                                    className={style.user__avatar}
+                                    image={article?.user?.avatar || ''}
+                                    borderRadius='50%'
+                                    alt={article?.title}
+                                />
+                                <span className={style.user__name}>{article?.user?.username}</span>
+                            </div>
+                            <div className={style.header__date}>{article?.createdAt}</div>
+                        </div>
+
+                        <h3 className={style.title}>{article?.title}</h3>
+
+                        {Types}
+
+                        <div className={style.img__container}>
+                            <img className={style.img} src={article?.img} alt={article?.title} />
+                        </div>
+
+                        <div className={style.content}>
+                            {contentBlock && (
+                                <ArticleTextBlock content={contentBlock as IArticleTextBlock} />
+                            )}
+                        </div>
+
+                        <div className={style.footer}>
+                            <Button label={t('Читать далее...')} />
+
+                            {Views}
+                        </div>
+                    </div>
+                </Card>
             </div>
         );
     }
 
     return (
-        <div className={classNames(style.item, [className])}>
+        <div className={classNames(style.item, [className, style[view]])}>
             <Card>
                 <div className={style.item__container}>
                     <div className={style.img__container}>
@@ -32,11 +93,8 @@ export const ArticleListItem = memo((props: ArticleTextBlockProps) => {
                     </div>
                     <div className={style.info__container}>
                         <div className={style.info}>
-                            <div className={style.info__type}>{article?.type}</div>
-                            <div className={style.info__views}>
-                                <div className={style.info__views_count}>{article.views}</div>
-                                <EyeIcon className={style.info__views_icon} />
-                            </div>
+                            {Types}
+                            {Views}
                         </div>
                         <div className={style.title}>{article?.title}</div>
                     </div>
